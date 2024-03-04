9f8c4a7d89fb56103be101795f6cd43cd0b03842
Svygzhryr <pamixy@gmail.com>
2024-03-04 15:13:46 +0300

    feat: add depth rule check

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,7 +1,8 @@
+import depthLimit from 'graphql-depth-limit';
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
+import { GraphQLArgs, Source, graphql, parse, validate } from 'graphql';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
 import { gqlSchema } from './mySchemas.js';
-import { graphql } from 'graphql';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   fastify.route({
@@ -13,21 +14,19 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
         200: gqlResponseSchema,
       },
     },
-
     async handler(req) {
       const schema = gqlSchema;
       const { query, variables } = req.body;
+      const ast = parse(new Source(query, 'General.graphql'));
+      const isQueryTooDeep = validate(schema, ast, [depthLimit(5)]);
       const response = await graphql({
         schema,
         source: query,
         variableValues: variables,
         contextValue: fastify.prisma,
-      }).then((response) => {
-        const { data, errors } = response;
-        return { data, errors };
       });
-
-      return response;
+      const { data, errors } = response;
+      return { data, errors: isQueryTooDeep || errors };
     },
   });
 };
